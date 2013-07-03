c67bfc3f870a5cb743a30582df5e0f6d77a701e1
Emanuele Mazza <n3m3da@d3cod3.org>
2013-07-03 13:07:13 +0100

    Added CV panel window

---
+++ src/gaCVModule.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxNSWindowApp.h"
+#include "ofxControlPanel.h"
+#include "gamuzaMain.h"
+
+extern gamuzaMain   *gapp; // external reference to gamuza app, declared in AppDelegate.h
+
+class gaCVModule : public ofxNSWindowApp {
+	
+public:
+    
+	gaCVModule(int windowW, int windowH);
+    ~gaCVModule();
+	
+	void setup();
+	void update();
+	void draw();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased();
+	void mouseReleased(int x, int y, int button);
+	void mouseScrolled(float x, float y);
+    
+    void setupGui();
+    void updateGui();
+    void drawGui();
+    
+    void restart();
+    
+    ofxControlPanel             gui;
+    simpleColor					gamuzaMainColor;
+    simpleColor					gamuzaWhiteColor;
+    simpleColor					gamuzaMarkColor;
+    simpleColor					gamuzaChartBackColor;
+    ofTrueTypeFont				fontSmall;
+    
+    int                         winW, winH;
+	
+};
