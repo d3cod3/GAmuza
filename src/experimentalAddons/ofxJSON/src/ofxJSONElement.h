1e080e15f6ebafdc5a7c427889273f4ac30c1d38
Emanuele Mazza <n3m3da@d3cod3.org>
2014-01-02 18:26:52 +0100

    added ofxJSON addon

---
+++ src/experimentalAddons/ofxJSON/src/ofxJSONElement.h
@@ -0,0 +1,36 @@
+/*
+ *  ofxJSONFile.h
+ *  asift
+ *
+ *  Created by Jeffrey Crouse on 12/17/10.
+ *  Copyright 2010 Eyebeam. All rights reserved.
+ *
+ *  Updates by Christopher Baker 2012 - 2013
+ *  http://christopherbaker.net
+ *
+ */
+
+
+#pragma once
+
+
+#include <string>
+#include "json.h"
+#include "ofLog.h"
+#include "ofURLFileLoader.h"
+
+
+class ofxJSONElement : public Json::Value {
+public:
+	ofxJSONElement() {};
+	ofxJSONElement(const std::string& jsonString);
+	ofxJSONElement(Json::Value& v);
+	
+	bool parse(const std::string& jsonString);
+	bool open(const std::string& filename);
+	bool openLocal(const std::string& filename);
+	bool openRemote(const std::string& filename);
+	bool save(const std::string& filename, bool pretty = false) const;
+	std::string getRawString(bool pretty = true) const;
+
+};
